00ddc07a5bf50c76f4432a4a0e3a9ea1e1cb6514
DuyChamHoc <duytran926@gmail.com>
2022-11-26 19:17:05 +0700

    navigation update

--- src/navigation/appStack.js
+++ src/navigation/appStack.js
@@ -1,44 +1,24 @@
 import React from 'react';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import Home from '../containers/home';
 import {colors} from '../global/styles';
 import Icon from 'react-native-vector-icons/FontAwesome';
 import Icon1 from 'react-native-vector-icons/AntDesign';
 import MyAccountScreen from '../containers/account/MyAccountScreen';
+import barnavigation from './barnavigation';
 const Tab = createBottomTabNavigator();
+const Stack = createNativeStackNavigator();
 export default function AppStack() {
   return (
-    <Tab.Navigator>
-      <Tab.Screen
-        name="home"
-        component={Home}
+    <Stack.Navigator>
+      <Stack.Screen
+        name="Bar"
+        component={barnavigation}
         options={{
           headerShown: false,
-          title: 'Home',
-          tabBarIcon: ({focussed, size}) => (
-            <Icon
-              name="home"
-              color={focussed ? '#7cc' : colors.grey2}
-              size={size}
-            />
-          ),
         }}
       />
-      <Tab.Screen
-        name="myaccount"
-        component={MyAccountScreen}
-        options={{
-          headerShown: false,
-          title: 'My Account',
-          tabBarIcon: ({focussed, size}) => (
-            <Icon1
-              name="user"
-              color={focussed ? '#7cc' : colors.grey2}
-              size={size}
-            />
-          ),
-        }}
-      />
-    </Tab.Navigator>
+    </Stack.Navigator>
   );
 }
