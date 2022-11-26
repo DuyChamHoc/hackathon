73ace04397e31f60b734918de634d7594c904cf7
DuyChamHoc <duytran926@gmail.com>
2022-11-26 18:42:00 +0700

    update user

--- src/navigation/appStack.js
+++ src/navigation/appStack.js
@@ -1,23 +1,42 @@
 import React from 'react';
-import SignInScreen from '../authScreens/SignInScreen';
-import SignInWelcomeScreen from '../authScreens/SignInWelcomeScreen';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import Home from '../containers/home';
+import {colors} from '../global/styles';
+import Icon from 'react-native-vector-icons/FontAwesome';
+import Icon1 from 'react-native-vector-icons/AntDesign';
+import MyAccountScreen from '../containers/account/MyAccountScreen';
 const Tab = createBottomTabNavigator();
 export default function AppStack() {
   return (
     <Tab.Navigator>
       <Tab.Screen
-        name="SignInScreen"
-        component={SignInScreen}
+        name="home"
+        component={Home}
         options={{
           headerShown: false,
+          title: 'Home',
+          tabBarIcon: ({focussed, size}) => (
+            <Icon
+              name="home"
+              color={focussed ? '#7cc' : colors.grey2}
+              size={size}
+            />
+          ),
         }}
       />
       <Tab.Screen
-        name="SignInWelcomeScreen"
-        component={SignInWelcomeScreen}
+        name="myaccount"
+        component={MyAccountScreen}
         options={{
           headerShown: false,
+          title: 'My Account',
+          tabBarIcon: ({focussed, size}) => (
+            <Icon1
+              name="user"
+              color={focussed ? '#7cc' : colors.grey2}
+              size={size}
+            />
+          ),
         }}
       />
     </Tab.Navigator>
