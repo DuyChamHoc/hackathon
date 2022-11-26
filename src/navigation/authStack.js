660955011c01968a4247630d629523e38b1a8e0b
DuyChamHoc <duytran926@gmail.com>
2022-11-26 15:30:43 +0700

    update login

--- src/navigation/authStack.js
+++ src/navigation/authStack.js
@@ -1,8 +1,7 @@
 import * as React from 'react';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import SignInWelcomeScreen from '../authScreens/SignInWelcomeScreen';
-import {View, Text} from 'react-native';
-// import SignInScreen from '../screens/authScreens/SignInScreen';
+import SignInScreen from '../authScreens/SignInScreen';
 // import SignUpScreen from '../screens/authScreens/SignUpScreen';
 /** */
 
@@ -12,16 +11,20 @@ export default function AuthStack() {
   return (
     <Stack.Navigator>
       <Stack.Screen
-        name="SignInWelcomeScreen"
-        component={SignInWelcomeScreen}
+        name="SignInScreen"
+        component={SignInScreen}
+        options={{
+          headerShown: false,
+        }}
       />
-      {/* <Auth.Screen
+      <Stack.Screen
         name="SignInWelcomeScreen"
         component={SignInWelcomeScreen}
         options={{
           headerShown: false,
         }}
-      /> */}
+      />
+
       {/* <Auth.Screen
         name="SignInScreen"
         component={SignInScreen}
